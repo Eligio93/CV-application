9a6750402def7ee6431a7f3ab849d146d9d664f1
Eligio93 <eligio.cristantielli@gmail.com>
2024-01-26 15:42:52 +1100

    using first lift state to render CV

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import GeneralInfoForm from './components/GeneralInfoForm';
+import RenderCV from './components/CvRender';
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [firstName,setFirstName]=useState("");
+  const [lastName,setLastName]=useState("");
+  function handleFirstName(e){
+    setFirstName(e.target.value);
+  }
+  function handlelastName(e){
+    setLastName(e.target.value);
+  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+//importare gli states e suddividere la pagine in layout
+return(
+  <>
+  <div className="input-section">
+<GeneralInfoForm
+  firstName={firstName}
+  LastName={lastName}
+  changeName={handleFirstName}
+  changeLastName={handlelastName}
+
+/>
+</div>
+<div className="rendering section">
+<RenderCV 
+  firstName={firstName}
+  lastName={lastName}
+  />
+</div>
+</>
+)
 }
 
 export default App
