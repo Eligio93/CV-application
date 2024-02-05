1f7feb5d781f9d795d7ff75529a9a7b243d9b821
Eligio93 <eligio.cristantielli@gmail.com>
2024-02-05 20:14:12 +1100

    add button to handle adding eduction

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,38 +1,40 @@
-import Input from "./Input"
+import Input from "./Input";
 
-
-export default function EducationForm({highSchool,subject,studyTitle,onChange}){
-
-    return(
-        <form id="education-form" className="form">
-        <h1>Education</h1>
-        <Input
-          label={"School:"}
-          type={"text"}
-          placeholder={"New York University"}
-          id={"School"}
-          value={highSchool}
-          onChange={onChange}
-        />
-        <Input
-          label={"Study Title:"}
-          type={"text"}
-          placeholder={"Degree, Diploma, Phd, Certification"}
-          id={"study-title"}
-          value={studyTitle}
-          onChange={onChange}
-        />
-        <Input
-          label={"Subject:"}
-          type={"text"}
-          placeholder={"Computer Science"}
-          id={"subject"}
-          value={subject}
-          onChange={onChange}
-        />
-        <button>Add</button>
-        <button>Remove</button>
-      </form>
-       
-    )
+export default function EducationForm({
+  school,
+  subject,
+  certificate,
+  onChange,
+  addEducation
+}) {
+  return (
+    <form id="education-form" className="form">
+      <h1>Education</h1>
+      <Input
+        label={"School:"}
+        type={"text"}
+        placeholder={"New York University"}
+        id={"school"}
+        value={school}
+        onChange={onChange}
+      />
+      <Input
+        label={"Study Title:"}
+        type={"text"}
+        placeholder={"Degree, Diploma, Phd, Certification"}
+        id={"certificate"}
+        value={certificate}
+        onChange={onChange}
+      />
+      <Input
+        label={"Subject:"}
+        type={"text"}
+        placeholder={"Computer Science"}
+        id={"subject"}
+        value={subject}
+        onChange={onChange}
+      />
+      <button onClick={addEducation}>Add Education</button>
+    </form>
+  );
 }
