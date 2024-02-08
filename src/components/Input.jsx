541bf264e15aa0aac2ee6a0b9d30a64360e5bd70
Eligio93 <eligio.cristantielli@gmail.com>
2024-02-08 14:42:25 +1100

    add prop to disable uinput field

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,4 @@
-export default function Input({ label, type, id, value, onChange,placeholder }) {
+export default function Input({ label, type, id, value, onChange,placeholder,disabled,checked}) {
     let pattern;
     if(type=="tel"){
       pattern='/^+[0-9]+$/'
@@ -7,7 +7,7 @@ export default function Input({ label, type, id, value, onChange,placeholder }) 
     <label htmlFor={id}>
       {label}
       <br />
-      <input type={type} id={id} value={value} onChange={onChange} pattern={pattern} placeholder={placeholder}/>
+      <input type={type} id={id} value={value} onChange={onChange} pattern={pattern} placeholder={placeholder} checked={checked} disabled={disabled}/>
     </label>
   );
 }
