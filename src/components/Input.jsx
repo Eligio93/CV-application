20c403f788e88f6d22e43f6038d3a8147985d57e
Eligio93 <eligio.cristantielli@gmail.com>
2024-02-08 18:08:01 +1100

    adds attribute for the checkbox

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -7,7 +7,7 @@ export default function Input({ label, type, id, value, onChange,placeholder,dis
     <label htmlFor={id}>
       {label}
       <br />
-      <input type={type} id={id} value={value} onChange={onChange} pattern={pattern} placeholder={placeholder} checked={checked} disabled={disabled}/>
+      <input type={type} id={id} value={value} onChange={onChange} pattern={pattern} placeholder={placeholder} checked={checked} disabled={disabled} required/>
     </label>
   );
 }
