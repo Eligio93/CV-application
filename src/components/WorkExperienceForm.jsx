4a52e2fc8b8ffa7afb8c6c49cf604cf971bd9104
Eligio93 <eligio.cristantielli@gmail.com>
2024-02-08 18:07:38 +1100

    passing still working prop for checkbox

--- src/components/WorkExperienceForm.jsx
+++ src/components/WorkExperienceForm.jsx
@@ -1,61 +1,66 @@
-import { useState } from "react";
 import Input from "./Input";
 
-
-export default function WorkExperienceForm({company,position,responsabilities,dateFrom,dateTo,onChange,addExperience}){
-   
-    const [showDate,setShowDate]=useState(true)
-    return(
-        <form className="form" id="work-experience-form">
-        <h1>Work Experience</h1>
-        <Input
-          label={"Company Name:"}
-          type={"text"}
-          placeholder={"Google"}
-          id={"company"}
-          value={company}
-          onChange={onChange}
-        />
-        <Input
-          label={"Position:"}
-          type={"text"}
-          placeholder={"Front End Developer"}
-          id={"position"}
-          value={position}
-          onChange={onChange}
-        />
-        <Input
-          label={"Responsabilities:"}
-          type={"textarea"}
-          id={"responsabilities"}
-          value={responsabilities}
-          onChange={onChange}
-        />
-        <Input
-          label={"From:"}
-          type={"date"}
-          id={"dateFrom"}
-          value={dateFrom}
-          onChange={onChange}
-        />
-        <Input
-          label={"To:"}
-          type={"date"}
-          id={"dateTo"}
-          value={dateTo}
-          onChange={onChange}
-          disabled={showDate}
-        />
-        <Input
-        label={"Still working here"}
+export default function WorkExperienceForm({
+  company,
+  position,
+  responsabilities,
+  dateFrom,
+  dateTo,
+  onChange,
+  addExperience,
+  stillWorking,
+  checkStillWorking
+}) {
+  
+  return (
+    <form className="form" id="work-experience-form">
+      <h1>Work Experience</h1>
+      <Input
+        label={"Company Name:"}
+        type={"text"}
+        placeholder={"Google"}
+        id={"company"}
+        value={company}
+        onChange={onChange}
+      />
+      <Input
+        label={"Position:"}
+        type={"text"}
+        placeholder={"Front End Developer"}
+        id={"position"}
+        value={position}
+        onChange={onChange}
+      />
+      <Input
+        label={"Responsabilities:"}
+        type={"textarea"}
+        id={"responsabilities"}
+        value={responsabilities}
+        onChange={onChange}
+      />
+      <Input
+        label={"From:"}
+        type={"date"}
+        id={"dateFrom"}
+        value={dateFrom}
+        onChange={onChange}
+      />
+      <Input
+        label={"To:"}
+        type={"date"}
+        id={"dateTo"}
+        value={dateTo}
+        onChange={onChange}
+        disabled={stillWorking}
+      />
+      <Input
+        label={"Still working here?"}
         type={"checkbox"}
-        id={"stillHere"}
-        checked={showDate}
-        onChange={(e)=>setShowDate(e.target.checked)}
-        />
-        <button onClick={addExperience}>Add Work Experience</button>
-       
-       
-      </form>
-    )
+        id={"stillWorking"}
+        checked={stillWorking}
+        onChange={checkStillWorking}
+      />
+      <button onClick={addExperience}>Add Work Experience</button>
+    </form>
+  );
 }
