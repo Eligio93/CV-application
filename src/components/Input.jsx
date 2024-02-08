242ce3ee44da59889d74c7b38948fbaedee31249
Eligio93 <eligio.cristantielli@gmail.com>
2024-02-08 19:13:59 +1100

    add condition for checkbox required

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,32 @@
-export default function Input({ label, type, id, value, onChange,placeholder,disabled,checked}) {
-    let pattern;
-    if(type=="tel"){
-      pattern='/^+[0-9]+$/'
-    }
+export default function Input({
+  label,
+  type,
+  id,
+  value,
+  onChange,
+  placeholder,
+  disabled,
+  checked,
+}) {
+  let pattern;
+  if (type == "tel") {
+    pattern = "/^+[0-9]+$/";
+  }
   return (
     <label htmlFor={id}>
       {label}
       <br />
-      <input type={type} id={id} value={value} onChange={onChange} pattern={pattern} placeholder={placeholder} checked={checked} disabled={disabled} required/>
+      <input
+        type={type}
+        id={id}
+        value={value}
+        onChange={onChange}
+        pattern={pattern}
+        placeholder={placeholder}
+        checked={checked}
+        disabled={disabled}
+        {...(type !== "checkbox" ? { required: true } : {})}
+      />
     </label>
   );
 }
