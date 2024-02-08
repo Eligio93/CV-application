0a6b11287f96d6778ff87ef020c60444ee9e793b
Eligio93 <eligio.cristantielli@gmail.com>
2024-02-08 14:43:04 +1100

    adds rendering of Education and Working Experience

--- src/components/CvRender.jsx
+++ src/components/CvRender.jsx
@@ -1,8 +1,8 @@
 import emailIcon from "/src/assets/email-icon.svg";
 import phoneIcon from "/src/assets/phone-icon.svg";
-import locationIcon from "/src/assets/location-icon.svg"
+import locationIcon from "/src/assets/location-icon.svg";
 
-function Header({...formValues}) {
+function Header({ ...formValues }) {
   return (
     <div className="header">
       <p>{formValues.firstName + " " + formValues.lastName}</p>
@@ -12,44 +12,79 @@ function Header({...formValues}) {
           {formValues.email}
         </div>
         <div>
-        <img src={phoneIcon} alt="" />
-          {formValues.phone}</div>
+          <img src={phoneIcon} alt="" />
+          {formValues.phone}
+        </div>
         <div>
-        <img src={locationIcon} alt="" />
-          {formValues.homeBase}</div>
+          <img src={locationIcon} alt="" />
+          {formValues.homeBase}
+        </div>
       </div>
     </div>
   );
 }
-function Sidebar({educationList,deleteEdu}){
-
-  return(
+function Sidebar({ educationList, deleteEdu, editEdu }) {
+  return (
     <div className="sidebar">
-       {educationList.map((education,index)=>(
+      {educationList.map((education, index) => (
         <div className="education-item" key={index}>
-        <button onClick={()=>deleteEdu(index)}>Delete</button>
-        <button >Edit</button>
-        <p>{education.certificate +' in '+education.subject}</p>
-        <p>{education.school}</p>
+          <button onClick={() => deleteEdu(index)}>Delete</button>
+          <button onClick={() => editEdu(index)}>Edit</button>
+          <p>
+            <b>{education.certificate + " in " + education.subject}</b>
+          </p>
+          <p>
+            <i>{education.school}</i>
+          </p>
         </div>
       ))}
+    </div>
+  );
+}
+function Content({experienceList}){
+  return(
+    <div className="content">
+      {experienceList.map((experience,index)=>(
+        <div className="experience-item" key={index}>
+        <button>Delete</button>
+        <button>Edit</button>
+        <p>
+          <b>{experience.position + " at " + experience.company}</b>
+        </p>
+        <p>
+          {experience.responsabilities}
+        </p>
+        {experience.dateTo==="" ? "Still working" : experience.dateTo}
+      </div>
+
+      ))
+      }
 
     </div>
   )
-  }
 
-export default function RenderCV({formValues,educationList,deleteEdu}) {
+}
+
+export default function RenderCV({
+  formValues,
+  educationList,
+  deleteEdu,
+  editEdu,
+  experienceList
+}) {
   return (
     <>
-      <Header
-       {...formValues}
-      />
-      <Sidebar
-      educationList={educationList}
-      deleteEdu={deleteEdu}
-      />
-     
-     
+      <Header {...formValues} />
+      <div id="main">
+        <Sidebar
+          educationList={educationList}
+          deleteEdu={deleteEdu}
+          editEdu={editEdu}
+          
+        />
+        <Content
+        experienceList={experienceList} />
+      </div>
     </>
   );
 }
